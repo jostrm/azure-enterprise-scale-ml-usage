4b6e79e215de9932d084ec16a0cf9cd9b8d75968
jostrm <jostrm@microsoft.com>
2025-01-02 21:54:50 +0100

    env

--- 10-mirror-gh-repo-from-template-once.sh
+++ 10-mirror-gh-repo-from-template-once.sh
@@ -9,24 +9,28 @@ NC='\033[0m' # No Color
 # 01. Setup and Preparation
 echo -e "${YELLOW}01. Copy environment variables locally to Github environments${NC}"
 
-# Read the variables from the bootstrap.properties file
-if [ -f ../bootstrap.properties ]; then
-    source ../bootstrap.properties
+# Read the variables from the .env file
+if [ -f ../.env ]; then
+    export $(grep -v '^#' ../.env | xargs)
+    if [ -z "$GITHUB_USERNAME" ]; then
+        echo -e "${RED}Failed to read the first variable,GITHUB_USERNAME, from .env.${NC}"
+        exit 1
+    fi
 else
-    echo -e "${RED}bootstrap.properties does not exist.${NC}"
+    echo -e "${RED}.env file does not exist.${NC}"
     exit 1
 fi
 
-github_your_repo_uri="https://github.com/${github_new_repo}.git"
+github_your_repo_uri="https://github.com/${GITHUB_NEW_REPO}.git"
 
-echo -e "\e[36mAZD Dev name:\e[0m $azd_dev_env_name"
+echo -e "\e[36mAZD Dev name:\e[0m $DEV_NAME"
 echo -e "\e[36mGitHub New Repo URI:\e[0m $github_your_repo_uri"
 
-gh api --method PUT -H "Accept: application/vnd.github+json" repos/$github_new_repo/environments/dev
-gh api --method POST -H "Accept: application/vnd.github+json" repos/$github_new_repo/environments/dev/variables -f name=AZURE_ENV_NAME -f value="$azd_dev_env_name"
-gh api --method POST -H "Accept: application/vnd.github+json" repos/$github_new_repo/environments/dev/variables -f name=AZURE_SUBSCRIPTION_ID -f value="$azd_dev_env_subscription"
-gh api --method POST -H "Accept: application/vnd.github+json" repos/$github_new_repo/environments/dev/variables -f name=AZURE_LOCATION -f value="$azd_dev_env_location"
-gh secret set AZURE_CREDENTIALS --repo $github_new_repo --env dev --body "replace_with_dev_sp_credencials"
+gh api --method PUT -H "Accept: application/vnd.github+json" repos/$GITHUB_NEW_REPO/environments/dev
+gh api --method POST -H "Accept: application/vnd.github+json" repos/$GITHUB_NEW_REPO/environments/dev/variables -f name=AZURE_ENV_NAME -f value="$DEV_NAME"
+gh api --method POST -H "Accept: application/vnd.github+json" repos/$GITHUB_NEW_REPO/environments/dev/variables -f name=AZURE_SUBSCRIPTION_ID -f value="$DEV_SUBSCRIPTION_ID"
+gh api --method POST -H "Accept: application/vnd.github+json" repos/$GITHUB_NEW_REPO/environments/dev/variables -f name=AZURE_LOCATION -f value="$AIFACTORY_LOCATION"
+gh secret set AZURE_CREDENTIALS --repo $GITHUB_NEW_REPO --env dev --body "replace_with_dev_sp_credencials"
 
 echo -e "${GREEN}02. Success!${NC}"
 
@@ -45,29 +49,29 @@ trap "rm -rf $temp_dir" EXIT
 cd "$temp_dir"
 
 # Setup additional variables
-if [ "$github_use_ssh" = "true" ]; then
-    github_template_repo_uri="git@github.com:${github_template_repo}.git"
+if [ "$GITHUB_USE_SSH" = "true" ]; then
+    github_template_repo_uri="git@github.com:${GITHUB_TEMPLATE_REPO}.git"
 else
-    github_template_repo_uri="https://github.com/${github_template_repo}.git"
+    github_template_repo_uri="https://github.com/${GITHUB_TEMPLATE_REPO}.git"
 fi
-if [ "$github_use_ssh" = "true" ]; then
-    github_new_repo_uri="git@github.com:${github_new_repo}.git"
+if [ "$GITHUB_USE_SSH" = "true" ]; then
+    github_new_repo_uri="git@github.com:${GITHUB_NEW_REPO}.git"
 else
-    github_new_repo_uri="https://github.com/${github_new_repo}.git"
+    github_new_repo_uri="https://github.com/${GITHUB_NEW_REPO}.git"
 fi
-github_new_repo_name=${github_new_repo##*/}
-github_template_repo_name=${github_template_repo##*/}
+github_new_repo_name=${GITHUB_NEW_REPO##*/}
+github_template_repo_name=${GITHUB_TEMPLATE_REPO##*/}
 
 echo -e "\e[33mBootstraping Parameters\e[0m"
 echo -e "\e[36mGitHub Username:\e[0m $github_username"
 echo -e "\e[36mGitHub Use SSH:\e[0m $github_use_ssh"
-echo -e "\e[36mGitHub Template Repo:\e[0m $github_template_repo"
+echo -e "\e[36mGitHub Template Repo:\e[0m $GITHUB_TEMPLATE_REPO"
 echo -e "\e[36mGitHub Template Repo name:\e[0m $github_template_repo_name"
 echo -e "\e[36mGitHub Template Repo URI:\e[0m $github_template_repo_uri"
-echo -e "\e[36mGitHub New Repo:\e[0m $github_new_repo"
+echo -e "\e[36mGitHub New Repo:\e[0m $GITHUB_NEW_REPO"
 echo -e "\e[36mGitHub New Repo name:\e[0m $github_new_repo_name"
 echo -e "\e[36mGitHub New Repo URI:\e[0m $github_new_repo_uri"
-echo -e "\e[36mGitHub New Repo Visibility:\e[0m $github_new_repo_visibility"
+echo -e "\e[36mGitHub New Repo Visibility:\e[0m $GITHUB_NEW_REPO_VISIBILITY"
 
 # 02. Repository Creation and Initialization
 echo -e "${YELLOW}02. New GitHub Repository Creation and Initialization.${NC}"
@@ -78,12 +82,12 @@ if [ -d "$new_project_repo" ]; then
 fi
 
 # Check if the repository already exists
-repo_exists=$(gh repo view "$github_new_repo" > /dev/null 2>&1; echo $?)
+repo_exists=$(gh repo view "$GITHUB_NEW_REPO" > /dev/null 2>&1; echo $?)
 
 if [ $repo_exists -ne 0 ]; then
     # Create a new GitHub repository
     echo -e "${YELLOW}Creating a new GitHub repository.${NC}"
-    gh repo create "$github_new_repo" --$github_new_repo_visibility
+    gh repo create "$GITHUB_NEW_REPO" --$GITHUB_NEW_REPO_VISIBILITY
     if [ $? -ne 0 ]; then
         echo -e "${RED}Failed to create new GitHub repository.${NC}"
         exit 1
@@ -100,11 +104,11 @@ cd $github_template_repo_name.git
 # Mirror-push to the new repository
 git push --mirror "$github_new_repo_uri"
 if [[ $? -ne 0 ]]; then
-  if [[ "$github_use_ssh" == "true" ]]; then
-    echo "ERROR: Permission denied to GitHub repo. github_use_ssh is true. Please look at this reference:"
+  if [[ "$GITHUB_USE_SSH" == "true" ]]; then
+    echo "ERROR: Permission denied to GitHub repo. GITHUB_USE_SSH is true. Please look at this reference:"
     echo "https://docs.github.com/en/get-started/getting-started-with-git/about-remote-repositories#cloning-with-https-urls"
   else
-    echo "ERROR: Permission denied to GitHub repo. github_use_ssh is false, you are using HTTPS. Please look at this reference:"
+    echo "ERROR: Permission denied to GitHub repo. GITHUB_USE_SSH is false, you are using HTTPS. Please look at this reference:"
     echo "https://docs.github.com/en/get-started/getting-started-with-git/about-remote-repositories#cloning-with-ssh-urls"    
   fi
   exit 1
@@ -123,11 +127,11 @@ else
 fi
 
 # Set the default branch to develop
-gh repo edit $github_new_repo --default-branch dev
+gh repo edit $GITHUB_NEW_REPO --default-branch dev
 
 # Setting default branch
 echo -e "${YELLOW}Setting default branch in the new repository.${NC}"
-gh repo edit $github_new_repo --default-branch dev
+gh repo edit $GITHUB_NEW_REPO --default-branch dev
 
 # dev branch protection rule
 # NOTE: removed to make it more flexible in the workshop
@@ -135,7 +139,7 @@ gh repo edit $github_new_repo --default-branch dev
 #   --method PUT \
 #   -H "Accept: application/vnd.github+json" \
 #   -H "X-GitHub-Api-Version: 2022-11-28" \
-#   repos/$github_new_repo/branches/dev/protection \
+#   repos/$GITHUB_NEW_REPO/branches/dev/protection \
 #   -F "required_status_checks[strict]=true" \
 #   -F "required_status_checks[contexts][]=evaluate-flow" \
 #   -F "enforce_admins=true" \
@@ -151,30 +155,30 @@ gh repo edit $github_new_repo --default-branch dev
 #   -F "allow_fork_syncing=true" \
 #   -F "restrictions=null"
 
-# Create GitHub environment named dev with specified variables
-gh api --method PUT -H "Accept: application/vnd.github+json" repos/$github_new_repo/environments/dev
-gh api --method POST -H "Accept: application/vnd.github+json" repos/$github_new_repo/environments/dev/variables -f name=AZURE_ENV_NAME -f value="$dev_name"
-gh api --method POST -H "Accept: application/vnd.github+json" repos/$github_new_repo/environments/dev/variables -f name=AZURE_SUBSCRIPTION_ID -f value="$dev_subscription_id"
-gh api --method POST -H "Accept: application/vnd.github+json" repos/$github_new_repo/environments/dev/variables -f name=AZURE_LOCATION -f value="$aifactory_location"
-gh secret set AZURE_CREDENTIALS --repo $github_new_repo --env dev --body "replace_with_dev_sp_credencials"
+# Create GitHub environment named DEV with specified variables
+gh api --method PUT -H "Accept: application/vnd.github+json" repos/$GITHUB_NEW_REPO/environments/dev
+gh api --method POST -H "Accept: application/vnd.github+json" repos/$GITHUB_NEW_REPO/environments/dev/variables -f name=AZURE_ENV_NAME -f value="$dev_name"
+gh api --method POST -H "Accept: application/vnd.github+json" repos/$GITHUB_NEW_REPO/environments/dev/variables -f name=AZURE_SUBSCRIPTION_ID -f value="$dev_subscription_id"
+gh api --method POST -H "Accept: application/vnd.github+json" repos/$GITHUB_NEW_REPO/environments/dev/variables -f name=AZURE_LOCATION -f value="$aifactory_location"
+gh secret set AZURE_CREDENTIALS --repo $GITHUB_NEW_REPO --env dev --body "replace_with_dev_sp_credencials"
 
-# Create placeholders for GitHub environment qa variables
-gh api --method PUT -H "Accept: application/vnd.github+json" repos/$github_new_repo/environments/stage
-gh api --method POST -H "Accept: application/vnd.github+json" repos/$github_new_repo/environments/stage/variables -f name=AZURE_ENV_NAME -f value="stage_name"
-gh api --method POST -H "Accept: application/vnd.github+json" repos/$github_new_repo/environments/stage/variables -f name=AZURE_SUBSCRIPTION_ID -f value="stage_subscription_id"
-gh api --method POST -H "Accept: application/vnd.github+json" repos/$github_new_repo/environments/stage/variables -f name=AZURE_LOCATION -f value="aifactory_location"
-gh secret set AZURE_CREDENTIALS --repo $github_new_repo --env qa --body "replace_with_qa_sp_credencials"
+# Create placeholders for GitHub environment STAGE variables
+gh api --method PUT -H "Accept: application/vnd.github+json" repos/$GITHUB_NEW_REPO/environments/stage
+gh api --method POST -H "Accept: application/vnd.github+json" repos/$GITHUB_NEW_REPO/environments/stage/variables -f name=AZURE_ENV_NAME -f value="stage_name"
+gh api --method POST -H "Accept: application/vnd.github+json" repos/$GITHUB_NEW_REPO/environments/stage/variables -f name=AZURE_SUBSCRIPTION_ID -f value="stage_subscription_id"
+gh api --method POST -H "Accept: application/vnd.github+json" repos/$GITHUB_NEW_REPO/environments/stage/variables -f name=AZURE_LOCATION -f value="aifactory_location"
+gh secret set AZURE_CREDENTIALS --repo $GITHUB_NEW_REPO --env qa --body "replace_with_stage_sp_credencials"
 
-# Create placeholders for GitHub environment prod variables
-gh api --method PUT -H "Accept: application/vnd.github+json" repos/$github_new_repo/environments/prod
-gh api --method POST -H "Accept: application/vnd.github+json" repos/$github_new_repo/environments/prod/variables -f name=AZURE_ENV_NAME -f value="prod_name"
-gh api --method POST -H "Accept: application/vnd.github+json" repos/$github_new_repo/environments/prod/variables -f name=AZURE_SUBSCRIPTION_ID -f value="prod_subscription_id"
-gh api --method POST -H "Accept: application/vnd.github+json" repos/$github_new_repo/environments/prod/variables -f name=AZURE_LOCATION -f value="aifactory_location"
-gh secret set AZURE_CREDENTIALS --repo $github_new_repo --env prod --body "replace_with_prod_sp_credencials"
+# Create placeholders for GitHub environment PROD variables
+gh api --method PUT -H "Accept: application/vnd.github+json" repos/$GITHUB_NEW_REPO/environments/prod
+gh api --method POST -H "Accept: application/vnd.github+json" repos/$GITHUB_NEW_REPO/environments/prod/variables -f name=AZURE_ENV_NAME -f value="prod_name"
+gh api --method POST -H "Accept: application/vnd.github+json" repos/$GITHUB_NEW_REPO/environments/prod/variables -f name=AZURE_SUBSCRIPTION_ID -f value="prod_subscription_id"
+gh api --method POST -H "Accept: application/vnd.github+json" repos/$GITHUB_NEW_REPO/environments/prod/variables -f name=AZURE_LOCATION -f value="aifactory_location"
+gh secret set AZURE_CREDENTIALS --repo $GITHUB_NEW_REPO --env prod --body "replace_with_prod_sp_credencials"
 
 echo -e "${GREEN}New repository created successfully.${NC}"
 
-echo -e "${GREEN}Access your new repo in: \nhttps://github.com/$github_new_repo ${NC}"
+echo -e "${GREEN}Access your new repo in: \nhttps://github.com/$GITHUB_NEW_REPO ${NC}"
 
 # Clone the new repository
 echo -e "${YELLOW}Cloning the new GitHub repository${NC}"
